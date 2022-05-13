5151d4de0697e899434540477eb5af202a2b768d
Olivia <laconte.olivia@sfr.fr>
2022-05-13 10:55:49 +0200

    ajout components pour html

---
+++ src/app/app-page-accueil/app-page-accueil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-page-accueil',
+  templateUrl: './app-page-accueil.component.html',
+  styleUrls: ['./app-page-accueil.component.scss']
+})
+export class AppPageAccueilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppPageAccueilComponent } from './app-page-accueil/app-page-accueil.component';
+import { AppGenerateurComponent } from './app-generateur/app-generateur.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppPageAccueilComponent,
+    AppGenerateurComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
